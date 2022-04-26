e3b871ca9ee100ace29c121b1870afb8bd4914e1
MeriemCh <chkirmeriem619@gmail.com>
2022-04-26 01:36:32 +0100

    Add files via upload
    Page d'accueil

---
+++ Acc.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FREECH</title>
+    <link rel="shortcut icon" href="icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="Acc.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                <i class="fas fa-gamepad"></i>
+                <p>FREECH</p>
+            </div>
+            <ul class="navbar">
+                <li><a href="../Logout.php" class="active">Log out</a></li>
+                <li><a href="../page.php" class="active">Games</a></li>
+
+            </ul>
+        </header>
+        <div class="content-wrapper">
+            <div class="content-desc">
+                <h1>FREECH</h1>
+                    <p>Welcome to <span>Freech</span> the place where you will be amazed by our games.</p>
+                    <p> Let's start playing!</p>
+                    <a href="../page.php">PLAY</a>
+            </div>
+            <div class="img">
+                <img src="yoss.png" width="400px" height="400px"></img>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-body">
+            <ul>
+                <h3>Short Links</h3>
+                <li><a href="#">  Sign in</a></li>
+                <li><a href="#">  Sign up</a></li>
+            </ul>
+            <ul>
+                <h3>Get In Touch</h3>
+                <li><a href="#"> Facebook</a></li>
+                <li><a href="#"> Instagram</a></li>
+                <li><a href="#"> Twitter</a></li>
+                <li><a href="#"> Linkedin</a></li>
+            </ul>
+        </div>
+    </footer>
+    <div class="footer-bottom">
+        <p>Copyright &copy; All Right Reserved | Designed <span>By Freech Team</span> 2022</p>
+    </div>
+</body>
+</html>
